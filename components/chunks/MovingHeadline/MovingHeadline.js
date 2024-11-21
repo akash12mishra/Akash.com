dde19553d6a0121f46f2a99123a294b38da4fe48
arkalal <arkalal.123@gmail.com>
2024-11-21 22:52:22 +0530

    moving headline setup is done

---
+++ components/chunks/MovingHeadline/MovingHeadline.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./MovingHeadline.module.scss";
+
+const MovingHeadline = () => {
+  const sentence = (
+    <>
+      <span className={`${styles.word} ${styles.filled}`}>NEXT</span>
+      <span className={`${styles.word} ${styles.bordered}`}>APPLICATION</span>
+      <span className={`${styles.word} ${styles.filled}`}>DEVELOPMENT</span>
+      <span className={`${styles.word} ${styles.bordered}`}>DESIGN</span>
+      <span className={`${styles.word} ${styles.filled}`}>PLATFORM</span>
+    </>
+  );
+
+  return (
+    <div className={styles.MovingHeadline}>
+      <div className={styles.movingText}>
+        {sentence}
+        {sentence} {/* Duplicate sentence to create a smooth loop */}
+      </div>
+    </div>
+  );
+};
+
+export default MovingHeadline;
--- components/pages/Home/Home.js
+++ components/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../../chunks/Navbar/Navbar";
+import MovingHeadline from "../../chunks/MovingHeadline/MovingHeadline";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <MovingHeadline />
     </div>
   );
 };
