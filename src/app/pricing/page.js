96abbe346bea5ec6700712db88f4580d212a92f0
arkalal <arkalal.123@gmail.com>
2024-12-10 00:02:14 +0530

    integrating lemonsqueezy

---
+++ src/app/pricing/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Price from "../../../components/pages/Price/Price";
+
+const page = () => {
+  return (
+    <div>
+      <Price />
+    </div>
+  );
+};
+
+export default page;
