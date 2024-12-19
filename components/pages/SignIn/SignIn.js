3a5a0d562e1c9c67d6942d86f387843ad18bb5dc
arkalal <arkalal.123@gmail.com>
2024-12-20 00:33:22 +0530

    building signin

---
+++ components/pages/SignIn/SignIn.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./SignIn.module.scss";
+
+const SignIn = () => {
+  return (
+    <div className={styles.SignIn}>
+      <h2>Lets do SIgnIN!</h2>
+    </div>
+  );
+};
+
+export default SignIn;
