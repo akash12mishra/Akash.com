eb685e205aa3f78f911459e60fd473ae304ab627
arkalal <arkalal.123@gmail.com>
2024-12-27 12:35:49 +0530

    working on user auth UI

--- components/chunks/Navbar/Navbar.js
+++ components/chunks/Navbar/Navbar.js
@@ -3,8 +3,14 @@ import styles from "./Navbar.module.scss";
 import Image from "next/image";
 import logoImg from "../../../assets/images/arka.png";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { useSession } from "next-auth/react";
+import UserBox from "../../UserBox/UserBox";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+
   return (
     <div className={styles.Navbar}>
       <div className={styles.navLogo}>
@@ -12,11 +18,33 @@ const Navbar = () => {
       </div>
 
       <div className={styles.authBtn}>
-        <button>
-          <span>Sign In</span>{" "}
-          <FaArrowRightLong className={styles.signInArrow} />{" "}
-        </button>
+        {session ? (
+          <>
+            <Image
+              src={session.user.image}
+              alt="logo"
+              className={styles.navLogoImg}
+              width={50}
+              height={50}
+            />
+          </>
+        ) : (
+          <>
+            <button onClick={() => router.push("/signIn")}>
+              <span>Sign In</span>{" "}
+              <FaArrowRightLong className={styles.signInArrow} />{" "}
+            </button>
+          </>
+        )}
       </div>
+
+      {session && (
+        <>
+          <div className={styles.userBoxWrapper}>
+            <UserBox />
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- components/pages/SignIn/SignIn.js
+++ components/pages/SignIn/SignIn.js
@@ -1,34 +1,51 @@
 "use client";
 
-import React from "react";
-import { signIn, signOut, useSession } from "next-auth/react";
+import React, { useEffect } from "react";
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import styles from "./SignIn.module.scss";
-import sampleImg from "../../../assets/images/codeService.png";
+import codeImg from "../../../assets/images/codeService.png";
+import arkaImg from "../../../assets/images/arka.png";
+import { FaGoogle } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 
 const SignIn = () => {
   const { data: session } = useSession();
+  const router = useRouter();
 
   console.log("session", session);
 
+  useEffect(() => {
+    if (session) {
+      router.push("/"); // Redirect to home page if user is logged in
+    }
+  }, [router, session]);
+
   return (
-    <div>
-      {session ? (
-        <>
-          <p>Signed in as {session.user.email}</p>
+    <div className={styles.SignIn}>
+      <div className={styles.signInLeft}>
+        <div className={styles.signInLeftLogo}>
+          <Image className={styles.signInLog} src={arkaImg} alt="CodeService" />
+        </div>
 
-          <Image
-            width={50}
-            height={50}
-            src={session.user.image || sampleImg}
-            alt={session.user.name}
-          />
+        <div className={styles.signInLeftContent}>
+          <Image className={styles.signInImg} src={codeImg} alt="" />
+          <p>arkalalchakravarty.</p>
+        </div>
+      </div>
 
-          <button onClick={() => signOut()}>Sign Out</button>
-        </>
-      ) : (
-        <button onClick={() => signIn("google")}>Sign In with Google</button>
-      )}
+      <div className={styles.signInRight}>
+        <div className={styles.signInRightTitle}>
+          <h3>Sign in to your account</h3>
+        </div>
+
+        <div className={styles.signInRightbtn}>
+          <button onClick={() => signIn("google")}>
+            {" "}
+            <FaGoogle /> Google
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
