20e9bea90f6a130f594ea84171a8fc6965ab0b6c
arkalal <arkalal.123@gmail.com>
2025-01-10 19:31:52 +0530

    fixing analytics

--- utils/GoogleAnalytics.js.js
+++ utils/GoogleAnalytics.js.js
@@ -1,27 +1,23 @@
 "use client";
-
 import Script from "next/script";
 
 export default function GoogleAnalytics({ GA_MEASUREMENT_ID }) {
   if (!GA_MEASUREMENT_ID) return null;
-
   return (
     <>
       <Script
-        strategy="afterInteractive"
+        async
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
       />
       <Script
         id="google-analytics"
-        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-            });
+
+            gtag('config', '${GA_MEASUREMENT_ID}');
           `,
         }}
       />
