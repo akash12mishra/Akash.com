d80c3c56b241b2f502b19326ee1cc0448db7dd89
arkalal <arkalal.123@gmail.com>
2025-01-11 12:51:47 +0530

    updated SEO

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,11 +1,14 @@
 export default async function sitemap() {
   const baseUrl = "https://arkalalchakravarty.com";
-
-  // Add all your static pages
-  const routes = ["", "/chatPlay"].map((route) => ({
+  const routes = [
+    "",
+    "/chatPlay",
+    "/signIn",
+    "/privacyPolicy",
+    "/termsOfService",
+  ].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString(),
   }));
-
   return routes;
 }
